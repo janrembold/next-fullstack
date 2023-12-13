9178a5a3d9e300fa9f26e2b7f10737ec77a4280d
janrembold <janrembold@gmx.net>
2023-12-13 15:05:33 +0100

    init

---
+++ src/app/api/prisma/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const now = Date.now();
+  await prisma.user.create({
+    data: {
+      email: `${now}@example.com`,
+      name: `${now}`,
+    },
+  });
+
+  const users = await prisma.user.findMany({
+    orderBy: {
+      name: "desc",
+    },
+    take: 5,
+  });
+
+  return NextResponse.json({ hello: "world", users });
+}
