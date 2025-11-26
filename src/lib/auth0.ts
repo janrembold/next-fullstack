2378aa94b79a91568d7d574e0dba6813aa4fd125
Jan Rembold <janrembold@gmail.com>
2025-11-26 14:32:44 +0100

    add auth0 callback for user creation

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,23 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server'
+import { NextResponse } from 'next/server'
 
-export const auth0 = new Auth0Client()
+export const auth0 = new Auth0Client({
+  async onCallback(error, context, session) {
+    // redirect the user to a custom error page
+    if (error)
+      return NextResponse.redirect(
+        new URL(`/error?error=${error.message}`, process.env.APP_BASE_URL),
+      )
+
+    console.log('Auth0 callback context:', { context, session })
+    if (session?.user?.sub) {
+      // create user in DB if not existing
+      // await createUserIfNotExists(session.user);
+    }
+
+    // complete the redirect to the provided returnTo URL
+    return NextResponse.redirect(
+      new URL(context.returnTo || '/', process.env.APP_BASE_URL),
+    )
+  },
+})
