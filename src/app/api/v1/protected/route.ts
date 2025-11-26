7b533d9bbda53962174bc06e4b0f747fd9448eab
Jan Rembold <janrembold@gmail.com>
2025-11-26 13:33:23 +0100

    Entferne nicht verwendete Dateien und aktualisiere Abhängigkeiten in package.json und pnpm-lock.yaml

--- src/app/api/v1/protected/route.ts
+++ src/app/api/v1/protected/route.ts
@@ -1,7 +1,12 @@
-import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0'
+import { auth0 } from '@/lib/auth0'
 import { NextResponse } from 'next/server'
 
-export const GET = withApiAuthRequired(async () => {
-    const session = await getSession()
+export const GET = async () => {
+    const session = await auth0.getSession()
+
+    // add protection logic as needed
+    if (!session)
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+
     return NextResponse.json(session?.user)
-})
+}
